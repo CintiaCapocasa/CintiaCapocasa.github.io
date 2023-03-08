8958cb816e1e50dd1f4d9dfc80471a36128b00e7
CintiaCapocasa <123754921+CintiaCapocasa@users.noreply.github.com>
2023-03-08 00:43:10 -0300

    Add files via upload

---
+++ Front end 2022/JS/function.js
@@ -0,0 +1,9 @@
+document. getElementById ('demo').innerHTML=  "Soy una persona con muchas ganas de aprender cosas nuevas y progresar"
+document.getElementById('boton_color').addEventListener('click', function (){document.body.style.backgroundColor=FF99CC;
+});
+document.getElementById('boton_default').addEventListener('click', function (){document.body.style.backgroundColor= "aqua";
+
+document.addEventListener('click', function(){
+       console.log(Soy la candidata que busca);
+       document.getElementById('demo'.innerHTML=Tengo muchas ganas de aprender)}
+}
